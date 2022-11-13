877e429d74a6130904389c988f76191c3f37ee07
Saidamir Sharofuddinov <amir@aprimerose.com>
2022-11-13 20:11:46 +0100

    running_sum_arr_v2

--- leetcode75/prefix-sum/running_sum_arr.py
+++ leetcode75/prefix-sum/running_sum_arr.py
@@ -1,3 +1,4 @@
+# 1
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         ans = [0] * len(nums)
@@ -5,3 +6,10 @@ class Solution:
         for i in range(1, len(nums)):
             ans[i] = ans[i-1] + nums[i]
         return ans
+
+# 2
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+             nums[i] += nums[i-1]
+        return nums
